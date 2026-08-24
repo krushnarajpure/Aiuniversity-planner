150f5aa12fc62747a7209faae8e422d1afc404c5
Krushna Rajpure <krushnarajpure>
2026-08-24 12:38:41 +0530

    Use stable local Next build cache

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: ".next-local",
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "**" },
